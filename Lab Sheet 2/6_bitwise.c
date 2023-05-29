17ea69a96ee8d6acc38b7aac6861c4e2300b5f2b
jrTlak <tilakthapa9898@gmail.com>
2023-05-29 05:49:06 +0545

    flowchart and algorithm removed

--- Lab Sheet 2/6_bitwise.c
+++ Lab Sheet 2/6_bitwise.c
@@ -6,8 +6,9 @@ int main()
     printf("a&b = %d\n", a & b);
     printf("a|b = %d\n", a | b);
     printf("a^b = %d\n", a ^ b);
-    printf("~a = %d\n", ~a);
-    printf("(b<<2)+(a<<1) = %d\n", (b << 2) + (a << 1));
-    printf("(b>>1)+(a>>1) = %d\n", (b >> 1) + (a >> 1));
+    printf("(a<<b) = %d\n", (a << 2));
+    printf("(b<<a) = %d\n", (b << 2));
+    printf("(a>>b) = %d\n", (a >> 2));
+    printf("(b>>a) = %d\n", (b >> 2));
     return 0;
 }
